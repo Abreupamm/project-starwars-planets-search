3f6f41050d2bba9f0bb0cc060d8ab9b04d18417c
Pamela <abrepamm@gmail.com>
2022-07-08 15:48:12 -0300

    projeto iniciado

---
+++ src/components/Table.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <table>
+      <tr>
+        <td>Name</td>
+        <td>Rotation Period</td>
+        <td>Orbital Period</td>
+        <td>Diameter</td>
+        <td>Climate</td>
+        <td>Gravity</td>
+        <td>Terrain</td>
+        <td>Surface Water</td>
+        <td>Population</td>
+        <td>Films</td>
+        <td>Created</td>
+        <td>Edited</td>
+        <td>URL</td>
+      </tr>
+    </table>
+  );
+}
+
+export default Table;
